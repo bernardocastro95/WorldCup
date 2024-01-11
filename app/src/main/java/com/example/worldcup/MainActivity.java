85c8f89a9acb3d9cf6a98edf3001bb0bf35d7d6c
bernardocastro95 <bernardoc544@gmail.com>
2024-01-10 20:43:14 -0500

    final validation and getInfo method partially working

--- app/src/main/java/com/example/worldcup/MainActivity.java
+++ app/src/main/java/com/example/worldcup/MainActivity.java
@@ -47,22 +47,24 @@ public class MainActivity extends AppCompatActivity {
 
                 et.getText();
                 checkInput();
+                inBetween();
                 //emptyInput();
                 checkOlympicYear();
                 checkWW2Year();
+                getInfo();
 
             }
         });
     }
 
     private void getInfo(){
-        DocumentReference dr = db.collection("worldcup").document(cup_id);
+        DocumentReference dr = db.collection("worldcup").document("worldcup");
         dr.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                 tv.setText("The World Cup of " + value.getString("year") + " was hosted at "
                 + value.getString("host") + ". It was won by " + value.getString("champion")
-                + "and the runner up was " + value.getString("runnerup") + ". The striker was " +
+                + " and the runner up was " + value.getString("runnerup") + ". The striker was " +
                         value.getString("striker"));
             }
         });
@@ -70,9 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkInput(){
          int inputValue = Integer.parseInt(et.getText().toString());
-         /*if(inputValue % 4 != 0){
-             Toast.makeText(this, "There was no World Cup in this year", Toast.LENGTH_SHORT).show();
-         }*/
+
          if(inputValue < 1930){
              Toast.makeText(this, "There were no World Cups before 1930", Toast.LENGTH_SHORT).show();
          }
@@ -84,7 +84,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*private void emptyInput(){
-        if(TextUtils.isEmpty(et.getText().toString())){
+        String userInput = et.getText().toString();
+        if(userInput.matches("")){
             Toast.makeText(this, "You must insert an input", Toast.LENGTH_SHORT).show();
         }
     }*/
@@ -107,6 +108,36 @@ public class MainActivity extends AppCompatActivity {
         int inputValue = Integer.parseInt(et.getText().toString());
         if(inputValue == 1942 || inputValue == 1946){
             Toast.makeText(this, "Due to WW2, there was no World Cup", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void inBetween(){
+        int inputValue = Integer.parseInt(et.getText().toString());
+        if(inputValue == 1931 || inputValue == 1933
+                || inputValue == 1935 || inputValue == 1937
+                || inputValue == 1939 || inputValue == 1941
+                || inputValue == 1943 || inputValue == 1945
+                || inputValue == 1946 || inputValue == 1947
+                || inputValue == 1948 || inputValue == 1949
+                || inputValue == 1951 || inputValue == 1953
+                || inputValue == 1955 || inputValue == 1957
+                || inputValue == 1959 || inputValue == 1961
+                || inputValue == 1963 || inputValue == 1965
+                || inputValue == 1967 || inputValue == 1969
+                || inputValue == 1971 || inputValue == 1973
+                || inputValue == 1975 || inputValue == 1977
+                || inputValue == 1979 || inputValue == 1981
+                || inputValue == 1983 || inputValue == 1985
+                || inputValue == 1987 || inputValue == 1993
+                || inputValue == 1995 || inputValue == 1997
+                || inputValue == 1999 || inputValue == 2001
+                || inputValue == 2003 || inputValue == 2005
+                || inputValue == 2007 || inputValue == 2009
+                || inputValue == 2011 || inputValue == 2013
+                || inputValue == 2015 || inputValue == 2017
+                || inputValue == 2019 || inputValue == 2021){
+            Toast.makeText(this, "There was no World Cup this year", Toast.LENGTH_SHORT).show();
+
+
         }
     }
 }
